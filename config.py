b6911e2ed1244ae5f22663c4f6de519c74db780e
salavat <salavat45zaynulin@gmail.com>
2024-06-30 19:52:30 +0300

    correct token

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = '5997119598:AAFCX3u9hrn9XZwAUrZwHTHlbW72k2sQur0'
+token = '7133946999:AAEV5Ld4Cl4i61378OwpkLEkJYBG-a0IhMk'
 # token_test = "5612789759:AAGW8CQr68S2MfbEl0ZqPkds3KsG55yZfRg"
 # token = token_test
 admins = [
