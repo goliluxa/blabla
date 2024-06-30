85cb2e88b0b6757bef1f1ae627def687f4077213
goliluxa <goliluxa@yandex.ru>
2024-06-30 16:04:09 +0300

    добавлен конфинг файл в репозиторий

---
+++ config.py
@@ -0,0 +1,7 @@
+token = '7133946999:AAEV5Ld4Cl4i61378OwpkLEkJYBG-a0IhMk'
+# token_test = "5612789759:AAGW8CQr68S2MfbEl0ZqPkds3KsG55yZfRg"
+# token = token_test
+admins = [
+    1215162451,
+    704665657
+]
